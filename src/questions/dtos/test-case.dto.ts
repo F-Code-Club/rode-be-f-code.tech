0d862669884f52da854e2d8a0f6135fe239c0540
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2024-06-13 22:05:57 +0700

    fix: resolve swagger req body

--- src/questions/dtos/test-case.dto.ts
+++ src/questions/dtos/test-case.dto.ts
@@ -2,9 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateTestCaseDto {
+  @ApiProperty()
   @IsNotEmpty()
   input: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   output: string;
 }
