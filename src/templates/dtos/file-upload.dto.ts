e737c464df0e894b533e967dbd875cced2238893
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2024-06-18 20:57:26 +0700

    fix: enable empty colorCode on swagger template

--- src/templates/dtos/file-upload.dto.ts
+++ src/templates/dtos/file-upload.dto.ts
@@ -3,6 +3,6 @@ import { ApiProperty } from '@nestjs/swagger';
 export class FileUploadDto {
   @ApiProperty({ type: 'string', format: 'binary' })
   file: Express.Multer.File;
-  @ApiProperty()
+  @ApiProperty({ required: false })
   colorCode: string;
 }
